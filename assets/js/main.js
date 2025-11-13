3bf2fce8e7faa03de0bfecfa5bb5baec762eaae2
miracle <liegeubani@gmail.com>
2025-11-13 23:12:06 +0100

    fix ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,12 @@ slides.addEventListener("touchend", () => {
 
   updateSlider();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
